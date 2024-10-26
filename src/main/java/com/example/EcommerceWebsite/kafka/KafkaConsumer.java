5bb5f0e9453560194cbfc8420c522ea18c0c4810
laraib-786 <ruk10011997@gmail.com>
2024-10-26 16:53:49 +0530

    Add kafka consumer integration

---
+++ src/main/java/com/example/EcommerceWebsite/kafka/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.example.EcommerceWebsite.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics="test-topic", groupId = "group_id")
+    public void consumeMessage(String message){
+
+        System.out.println("---------------------------------Received message: " + message);
+    }
+}
